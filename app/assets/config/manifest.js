c5df079e5d5ea7e96a3dac7368419845ade0b8ff
Nicholas Sellek <nsellek@protonmail.com>
2022-04-01 12:32:36 -0400

    Initial WebsiteAdmin configuration

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link website_admin/application.css
---
+++ config/initializers/website_admin.rb
@@ -0,0 +1,3 @@
+WebsiteAdmin.configure do |config|
+  config.database_url = ENV['WEBSITE_ADMIN_DATABASE_URL']
+end
